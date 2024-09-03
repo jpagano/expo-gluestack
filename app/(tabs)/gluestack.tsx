a3c0cdfabe08e974fd8ce22018d24ade8dd44512
jpagano <jordi.pagano@gmail.com>
2024-09-03 17:06:00 +0200

    Add gluestack-ui v2

---
+++ app/(tabs)/gluestack.tsx
@@ -0,0 +1,10 @@
+import { Center } from "@/components/ui/center";
+import { Text } from "@/components/ui/text";
+
+export default function GluestackScreen() {
+  return (
+    <Center className="h-full w-full bg-primary-500">
+      <Text className="font-bold text-typography-0">This is the center.</Text>
+    </Center>
+  );
+}
